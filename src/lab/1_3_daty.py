1f06f3ebc7d1e1510336e3ae949e0e37e31b0c0f
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2020-11-06 16:26:00 +0100

    delta

--- src/lab/1_3_daty.py
+++ src/lab/1_3_daty.py
@@ -2,7 +2,29 @@ data1 = (1, 4, 2020)
 data2 = (3, 4, 2020)
 
 ktora = 0
+if data1[2] < data2[2]:
+    ktora = 1
+else:
+    if data1[2] > data2[2]:
+        ktora = 2
+    else:
+        if data1[1] < data2[1]:
+            ktora = 1
+        else:
+            if data1[1] > data2[1]:
+                ktora = 2
+            else:
+                if data1[0] < data2[0]:
+                    ktora = 1
+                else:
+                    if data1[0] > data2[0]:
+                        ktora = 2
+                    else:
+                        ktora = 0
 
+print(ktora)
+
+ktora = 0
 if data1[2] < data2[2]:
     ktora = 1
 elif data1[2] > data2[2]:
---
+++ src/lab/1_4_delta.py
@@ -0,0 +1,36 @@
+import math
+
+(a, b, c) = (1, -5, 6)
+
+x1 = None
+x2 = None
+brak = None
+wiele = None
+
+if a != 0:
+    delta = b * b - 4 * a * c
+    if delta > 0:
+        x1 = (-b + math.sqrt(delta))/(2*a)
+        x2 = (-b - math.sqrt(delta))/(2*a)
+    else:
+        if delta < 0:
+            brak = True
+        else:
+            x1 = (-b * 1.0) / (2 * a )
+else:
+    if b != 0:
+        x1 = (-c * 1.0) / b
+    else:
+        if c != 0:
+            brak = True
+        else:
+            wiele = True
+
+if x1:
+    print("x1 =", x1)
+if x2:
+    print("x2 =", x2)
+if brak:
+    print("Brak")
+if wiele:
+    print("Wiele")
