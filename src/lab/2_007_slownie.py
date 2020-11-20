cf57c5b4aa2865075573c262f250089cce575ff9
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2020-11-20 19:52:09 +0100

    szyfr

--- src/lab/2_007_slownie.py
+++ src/lab/2_007_slownie.py
@@ -8,16 +8,16 @@ def number_to_list(number):
     return result
 
 
-# print(number_to_list(102))
+print(number_to_list(102))
 # print(number_to_list(1234567890))
 
-def number_list_to_names(numb_list):
-    numbers = ["zero", "jeden", "dwa",
-               "trzy", "cztery", "piec",
-               "szesc", "siedem", "osiem", "dziewiec"]
-    return [el for el in map((lambda x: numbers[x]), numb_list)]
-
-
-n = 1997
-print(number_list_to_names(number_to_list(n)))
-
+# def number_list_to_names(numb_list):
+#     numbers = ["zero", "jeden", "dwa",
+#                "trzy", "cztery", "piec",
+#                "szesc", "siedem", "osiem", "dziewiec"]
+#     return [el for el in map((lambda x: numbers[x]), numb_list)]
+#
+#
+# n = 1997
+# print(number_list_to_names(number_to_list(n)))
+#
---
+++ src/lab/2_008_szyfr.py
@@ -0,0 +1,17 @@
+def encrypt(text, shift=10):
+    result = ""
+    for i in range(len(text)):
+        char = text[i]
+        if char.isupper():
+            result += chr((ord(char) + shift - 65) % 26 + 65)
+        elif char.isspace():
+            result += char
+        else:
+            result += chr((ord(char) + shift - 97) % 26 + 97)
+    return result
+
+tekst = "na straganie w dzien targowy takie slyszy sie rozmowy"
+
+szyfr = encrypt(tekst, shift=2)
+
+print(szyfr)
